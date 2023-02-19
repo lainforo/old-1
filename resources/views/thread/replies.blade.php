75a4ebd287ca664d3032cd9dc6c01c4556ee435c
LostBox66 <lostbox66@protonmail.com>
2023-02-18 21:16:11 -0500

    Fix reply timestamp issue

--- resources/views/thread/replies.blade.php
+++ resources/views/thread/replies.blade.php
@@ -1,6 +1,6 @@
 @foreach ($replies as $reply)
 <div class="reply">
-        <span class="postername">{{ $reply->author }}</span> at {{ date("m/d/y (D) H:i:s", strtotime($thread->created_at)) }} No. {{ $reply->id }}
+        <span class="postername">{{ $reply->author }}</span> at {{ date("m/d/y (D) H:i:s", strtotime($reply->created_at)) }} No. {{ $reply->id }}
         @if (Cookie::get('admin_login') === env('LF_PASSWORD'))
             @include('form.modactions', ['postType' => 'reply'])
         @endif
