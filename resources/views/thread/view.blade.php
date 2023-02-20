004217fe6eae027890004e2bade25a303bd279f8
LostBox66 <lostbox66@protonmail.com>
2023-02-19 19:00:11 -0500

    Board icon on thread page

--- resources/views/thread/view.blade.php
+++ resources/views/thread/view.blade.php
@@ -12,6 +12,11 @@
         @include('board.nav')
         <br />
         <div class="boardlist noncenter">
+            @if ($board->iconpath == null)
+                <div class="center"><img src="{{ URL::asset('assets/img/board-default.png') }}" height=128></div><br />
+            @else
+                <div class="center"><img src="{{ $board->iconpath }}" height=128></div>
+            @endif
             <h1>{{ $thread->subject }}</h1>
             by <span class="postername">{{ $thread->author }}</span> at {{ date("m/d/y (D) H:i:s", strtotime($thread->created_at)) }}
             @if (Cookie::get('admin_login') === env('LF_PASSWORD'))
